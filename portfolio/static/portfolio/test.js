193484c60db35e262f7a20f30ad1b37cdf254260
TientjieIT <tientjieit@gmail.com>
2020-09-23 12:00:38 +0200

    navbar responsief

--- portfolio/static/portfolio/test.js
+++ portfolio/static/portfolio/test.js
@@ -1,8 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    function toggle() {
+      var navbar = document.getElementById("Basenav");
+      if (navbar.className === "base_nav") {
+      navbar.className += " responsive";
+      } else {
+      navbar.className = "base_nav";
+      }
   }
