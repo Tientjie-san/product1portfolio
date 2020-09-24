21ff8b8e8ac31dfc68e61f571b734380ef5a103e
TientjieIT <tientjieit@gmail.com>
2020-09-24 11:56:30 +0200

    portfolio pagina gallery gemaakt

--- portfolio/static/portfolio/util.js
+++ portfolio/static/portfolio/util.js
@@ -1,4 +1,4 @@
-    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    /* Toggle between adding and removing the "responsive" class to base_nav when the user clicks on the icon */
     function toggle() {
       var navbar = document.getElementById("Basenav");
       if (navbar.className === "base_nav") {
